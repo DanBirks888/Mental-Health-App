cc894b947221ad9da2281109d5361910e1f46f68
Dan Birks <63508057+DanBirks888@users.noreply.github.com>
2021-02-09 11:00:54 +0000

    Readme added and updated! :)

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const TextInANest = () => {
+  const title = useState("Dear Me");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  export default function App() {
+    return (
+      <View style={StyleSheet.title}>
+        <View>
+          <Text>Dear Me</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+  });
+};
