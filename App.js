71b6553f2695c087edee643b2d5dcd7e17d28730
Dan Birks <63508057+DanBirks888@users.noreply.github.com>
2021-02-10 09:41:23 +0000

    Close to functioning login screen! :D

--- App.js
+++ App.js
@@ -1,25 +1,75 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { cloneElement, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ImageBackground,
+  Image,
+  Alert,
+} from "react-native";
 
-const TextInANest = () => {
-  const title = useState("Dear Me");
+const images = [require("./images/leavesBackground.jpg")];
 
-  export default function App() {
-    return (
-      <View style={StyleSheet.title}>
-        <View>
-          <Text>Dear Me</Text>
+export default function App() {
+  return (
+    <ImageBackground source={images[0]} style={styles.backgroundImage}>
+      <View style={styles.pad}>
+        <Text style={styles.title}>Mental Health Application</Text>
+        <Text style={styles.normalText}>Log In</Text>
+        <View style={styles.loginInfo}>
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor="white"
+            style={styles.input}
+          />
+          <Button title="confirm" styles={styles.confirmBtn} />
         </View>
-        <StatusBar style="auto" />
       </View>
-    );
-  }
+      <StatusBar style="auto" />
+    </ImageBackground>
+  );
+}
 
-  const styles = StyleSheet.create({
-    title: {
-      fontSize: 20,
-      fontWeight: "bold",
-    },
-  });
-};
+let styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    height: undefined,
+    width: undefined,
+    resizeMode: "contain",
+  },
+  pad: {
+    padding: 50,
+  },
+  title: {
+    fontSize: 30,
+    paddingVertical: 20,
+    color: "white",
+    textAlign: "center",
+  },
+  normalText: {
+    fontSize: 20,
+    paddingVertical: 20,
+    color: "white",
+    textAlign: "center",
+  },
+  loginInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    color: "white",
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "white",
+  },
+  input: {
+    flex: 2,
+    color: "white",
+    placeholder: "Email",
+  },
+  confirmBtn: {
+    flex: 1,
+    backgroundColor: "rgba(52, 52, 52, 0.8)",
+  },
+});
